762ee12d2a60fd2d23663da59850e88f39d9f335
Ryan Day <ryandaydev@gmail.com>
2024-08-26 12:22:07 +0000

    Test deploying change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("My template APP")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Testing a change in the repo."
 )
